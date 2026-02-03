70bbb42c3cd0918210f6c1062629d152f14c24eb
gy212 <2124065319@qq.com>
2026-02-03 11:45:12 +0800

    main: 移除Qt版本文件

--- photowall-qt/include/photowall.h
+++
@@ -1,7 +0,0 @@
-#ifndef PHOTOWALL_QT_FFI_H
-#define PHOTOWALL_QT_FFI_H
-
-// Use the real PhotoWall FFI header generated in the Rust crate.
-#include "../../src-tauri/crates/photowall-ffi/include/photowall.h"
-
-#endif // PHOTOWALL_QT_FFI_H
